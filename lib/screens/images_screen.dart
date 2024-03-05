131ffa6c145e66841a597ea8c0f95c22bf8d7456
ifranco07 <i.francomtz07@gmail.com>
2024-03-05 12:01:35 -0600

    Imagenes

--- lib/screens/images_screen.dart
+++ lib/screens/images_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
+import 'package:practica3/theme/app_theme.dart';
 
 class ImagesScreen extends StatefulWidget {
   const ImagesScreen({super.key});
@@ -12,14 +14,66 @@ class _ImagesScreenState extends State<ImagesScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Imagenes'),
+        title: const Text('Imagenes'),
       ),
-      body: const Column( 
+      body: ListView( 
         children: [
-          Image(
-            image: AssetImage('assets/img/homer.png'))
+          cardImage1(),
+          image2(),   
         ],
       )
+    );
+  }
+  Card cardImage1() {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30),
+      ),
+      margin: const EdgeInsets.all(15),
+      elevation: 10,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30),
+        child:Column(
+        children: [
+          const SizedBox(
+            height: 450.0,
+            width: 900.0,
+            child: Image(
+              image: AssetImage('assets/img/homer.png')
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: Text(
+              'Homero',
+              style: AppTheme.lightTheme.textTheme.headlineLarge,
+            ),
+          ),
+        ],
+      ),
+    )
+    );
+  }
+  SizedBox image2() {
+    return SizedBox(
+      height: 200,
+      width: 200.0,
+      child: Image.network(
+        'https://i.ytimg.com/vi/jZqCkNEQ5HI/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBLv4VuZtjR5Tjjl0KE3N1vsodJuw'),
+    );   
+  }
+  Stack imageFade() {
+    return Stack(
+      children: <Widget>[
+        const Center(
+          child: CircularProgressIndicator(),
+        ),
+        Center(
+          child: FadeInImage.memoryNetwork(
+            placeholder: kTransparentImage, 
+            image: 'https://i.ytimg.com/vi/jZqCkNEQ5HI/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBLv4VuZtjR5Tjjl0KE3N1vsodJuw'),
+        )
+      ],
     );
   }
 }
